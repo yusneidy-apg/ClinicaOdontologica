4e88fadfeed96918c96bfb4984ee3290508b6cc0
harrymontenegro <harryjm018@hotmail.com>
2022-11-28 21:42:25 -0500

    Odontologo CRUD completo HQL

--- clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/repositorio/OdontologoRepositorio.java
+++ clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/repositorio/OdontologoRepositorio.java
@@ -2,9 +2,11 @@ package com.clinicaodontologica.app.clinicaodontologica.repositorio;
 
 import com.clinicaodontologica.app.clinicaodontologica.entities.Odontologo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OdontologoRepositorio extends JpaRepository<Odontologo, Integer> {
+    @Query("from Odontologo o where o.matricula = ?1")
     Odontologo findByMatricula(String matricula);
 }
