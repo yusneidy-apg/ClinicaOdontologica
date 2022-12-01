a88f44bab06666314b9e67d81c68fd8d2d2099a4
yusneidy-apg <yusneidypolanco4@gmail.com>
2022-11-30 21:23:06 -0500

    PacienteController

--- clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/controller/PacienteController.java
+++ clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/controller/PacienteController.java
@@ -1,28 +1,41 @@
 package com.clinicaodontologica.app.clinicaodontologica.controller;
 
 
+import com.clinicaodontologica.app.clinicaodontologica.dto.OdontologoDTO;
 import com.clinicaodontologica.app.clinicaodontologica.dto.PacienteDTO;
-import com.clinicaodontologica.app.clinicaodontologica.entities.Paciente;
 import com.clinicaodontologica.app.clinicaodontologica.servicios.PacienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/paciente")
-
 public class PacienteController {
-
     private final PacienteServicio pacienteServicio;
 
     @Autowired
     public PacienteController(PacienteServicio pacienteServicio) {
         this.pacienteServicio = pacienteServicio;
     }
+
+    @GetMapping
+    public List<PacienteDTO> buscarTodosLosPacientes() {
+        return pacienteServicio.listar();
+    }
+
+    @GetMapping("/{idPaciente}")
+    public PacienteDTO buscarPacientePorId(@PathVariable Integer idPaciente) {
+        return pacienteServicio.bucarPorId(idPaciente);
+    }
+
+    @GetMapping("/buscarPorIdPaciente/{idpaciente}")
+    public PacienteDTO buscarPacientePorIdPaciente(@PathVariable Integer idPaciente){ // podria buscarlo por DNI?
+        return pacienteServicio.bucarPorId(idPaciente);
+    }
+
+
     @PostMapping
     public PacienteDTO crearPaciente (@RequestBody PacienteDTO pacienteDTO) throws SQLException, ClassNotFoundException {
       return pacienteServicio.crear(pacienteDTO);
--- clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/repositorio/PacienteRepositorio.java
+++ clinicaodontologica/src/main/java/com/clinicaodontologica/app/clinicaodontologica/repositorio/PacienteRepositorio.java
@@ -2,8 +2,10 @@ package com.clinicaodontologica.app.clinicaodontologica.repositorio;
 
 import com.clinicaodontologica.app.clinicaodontologica.entities.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepositorio extends JpaRepository<Paciente, Integer> {
+
 }
